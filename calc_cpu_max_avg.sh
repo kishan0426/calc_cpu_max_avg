c1f272ab4ad9d53b11201b265a8181b1b80b62eb
kishan0426 <51046707+kishan0426@users.noreply.github.com>
2024-07-24 17:32:49 +0530

    Add files via upload

--- calc_cpu_max_avg.sh
+++ calc_cpu_max_avg.sh
@@ -9,8 +9,6 @@
 sardir=/var/log/sa
 cd $sardir
 d=$1
-var=0
-max=0
 if [[ $# -eq 0 ]]
 then
     echo "Provide arguments for number of days in d format"
@@ -20,6 +18,8 @@ if [[ $d -gt 30 ]]
 then
     echo "Provide number of days for report less than 31"
 else
+var=0
+max=0
 for cpu in `ls -lrt $sardir|grep 'sa'|grep -v 'sar'|tail -$d|awk '{print $NF}'`;
 do
     high=`sar -f $cpu|awk '{print $NF}'|grep -Eo "[0-9]+\.[0-9]+"|awk '{print 100 - $1}'|sort -nr|head -1`
@@ -28,7 +28,7 @@ then
     max=$high
 fi
 avgx=`sar -f $cpu|awk '{print $NF}'|grep -Eo "[0-9]+\.[0-9]+"|awk '{sum+=$1;} END{ if ( NR > 0 );{print 100-(sum/NR)};}'`
-avgy=`echo "$var + $avgx"|bc`
+#avgy=`echo "$var + $avgx"|bc`
 var=`echo "$var + $avgx"|bc`
 done
 res=`echo "$var / $d"|bc`
